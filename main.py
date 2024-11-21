8999dde587169e5d119d365e9d62c81bc7809776
Vanessa Liu <92833798+NessaLiu@users.noreply.github.com>
2024-11-21 21:45:31 +0800

    uncomment

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ staticBlocks = list()
 # tetris pieces
 current = Mino_T()
 current.setXY(settings.START_LOCATION_X, settings.START_LOCATION_Y)
-# next = Mino_L()
-# next.setXY(0, 0)
+next = Mino_L()
+next.setXY(0, 0)
 
 
 
@@ -64,8 +64,8 @@ while running:
     game_board.bilt(settings.WINDOW_WIDTH/2 - settings.GAME_WIDTH/2 - 10, settings.WINDOW_HEIGHT/2 - settings.GAME_HEIGHT/2 - 10)
     next_piece.bilt(settings.WINDOW_WIDTH/2 - settings.GAME_WIDTH * 1.5, settings.WINDOW_HEIGHT/2 + settings.GAME_HEIGHT/4 - 10)
     current.bilt()
-    # next.bilt()
-    # print(current.b[0].x, next.b[0].x)
+    next.bilt()
+    print(current.b[0].x, next.b[0].x)
     
     pygame.display.update()
     current.update()
